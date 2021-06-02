b1b6d3de542090ad78c98832a2c5df2688b18408
Tina Thompson <mark_junior13@yahoo.com>
2021-06-02 18:07:57 -0500

    added everything else. Done.

--- game.js
+++ game.js
@@ -1,62 +1,127 @@
 let options = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
+
+class InputInfo
+{
+    constructor(valid, playerChoice, computerChoice)
+    {
+        this.valid = valid;
+        this.playerChoice = playerChoice;
+        this.computerChoice = computerChoice;
+    }
+}
+
+function game()
+{
+    playerScore = 0;
+    computerScore = 0;
+    let roundsCompleted = 0;
+    do
+    {
+        let input = prompt("Choose rock, paper, or scissors");
+        let output = playRound(convertToOptionIndex(input), computerPlay());
+        if (!output)
+        {
+            console.log("Round could not be completed");
+        }
+        else
+        {
+            console.log(`Your Score: ${playerScore}, Computer Score: ${computerScore}`);
+            console.log(output);
+            roundsCompleted++;
+        }
+    } while (roundsCompleted < 5);
+
+    console.log("-----------------------------------");
+    console.log(`Your Score: ${playerScore}, Computer Score: ${computerScore}`);
+    if (playerScore > computerScore)
+    {
+        console.log("You win overall!");
+    }
+    else if (computerScore > playerScore)
+    {
+        console.log("Computer wins overall!");
+    }
+    else
+    {
+        console.log("It's a Draw!");
+    }
+}
 
 function computerPlay()
 {
-    let choice = Math.floor( Math.random() * 3);
+    let choice = Math.floor(Math.random() * 3);
     return choice;
-}
-
-
-function capitalize(text)
-{
-    return text[0].toUpperCase() + text.substr(1);
 }
 
 function playRound(playerSelection, computerSelection)
 {
     let playerSelectionIndex;
     let computerSelectionIndex;
-    if(typeof(playerSelection) === typeof("") && typeof(computerSelection) === typeof(""))
+    let info = new InputInfo(false, -1, -1);
+    checkValid(info, playerSelection, computerSelection);
+    if (info.valid)
     {
-        playerSelectionIndex = convertToOptionIndex(playerSelection);
-        computerSelectionIndex = convertToOptionIndex(computerSelection);
+        playerSelectionIndex = info.playerChoice;
+        computerSelectionIndex = info.computerChoice;
+        if (playerSelectionIndex === (computerSelectionIndex + 1) % 3)
+        {
+            playerScore++;
+            return `You Win! ${capitalize(options[playerSelectionIndex])} beats ${capitalize(options[computerSelectionIndex])}`;
+        }
+        else if (computerSelectionIndex === (playerSelectionIndex + 1) % 3)
+        {
+            computerScore++;
+            return `You Lose! ${capitalize(options[computerSelectionIndex])} beats ${capitalize(options[playerSelectionIndex])}`;
+        }
+        else
+        {
+            return `Tie Game! You both picked ${capitalize(options[playerSelectionIndex])}`;
+        }
     }
-    else if(typeof(playerSelection) === typeof(1) && typeof(computerSelection) === typeof(1))
+    return false;
+}
+
+function checkValid(info, playerSelection, computerSelection)
+{
+    let playerSelectionIndex;
+    let computerSelectionIndex;
+    if (typeof (playerSelection) === typeof ("") && typeof (computerSelection) === typeof (""))
     {
-        playerSelectionIndex = playerSelection;
-        computerSelectionIndex = computerSelection;
+        info.valid = true;
+        info.playerChoice = convertToOptionIndex(playerSelection);
+        info.computerChoice = convertToOptionIndex(computerSelection);
+    }
+    else if (typeof (playerSelection) === typeof (1) && typeof (computerSelection) === typeof (1))
+    {
+        info.valid = playerSelection >= 0 && computerSelection >= 0;
+        info.playerChoice = playerSelection;
+        info.computerChoice = computerSelection;
     }
     else
     {
-        return "Invalid Input";
+        console.log("Invalid Input");
+        info.valid = false;
+        return;
     }
 
-    if(playerSelectionIndex < 0 || computerSelectionIndex < 0)
+    if (playerSelectionIndex < 0 || computerSelectionIndex < 0)
     {
-        return "Invalid Input";
-    }
-
-    if( playerSelectionIndex === (computerSelectionIndex + 1)%3)
-    {
-        return `You Win! ${capitalize(options[playerSelectionIndex])} beats ${capitalize(options[computerSelectionIndex])}`;
-    }
-    else if(computerSelectionIndex === (playerSelectionIndex + 1)%3)
-    {
-        return `You Lose! ${capitalize(options[computerSelectionIndex])} beats ${capitalize(options[playerSelectionIndex])}`;
-    }
-    else
-    {
-        return `Tie Game! You both picked ${capitalize(options[playerSelectionIndex])}`;
+        console.log("Invalid Input");
+        info.valid = false;
+        return;
     }
 }
 
-function areValid(playerSelection, computerSelection)
+function capitalize(text)
 {
-    
+    return text[0].toUpperCase() + text.substr(1);
 }
 
 function convertToOptionIndex(optionString)
 {
+    optionString = optionString.trim();
     optionString = optionString.toLowerCase();
     return options.indexOf(optionString);
 }
